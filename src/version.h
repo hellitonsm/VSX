f77344dc00feb99a3bee7bc826567634a2da4bbf
VsyncCrypto <32690416+VsyncCrypto@users.noreply.github.com>
2018-02-13 18:33:22 -0500

    Network Protocol Version

--- src/version.h
+++ src/version.h
@@ -13,25 +13,21 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70710;
+static const int PROTOCOL_VERSION = 70715;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70077;
+static const int GETHEADERS_VERSION = 70715;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70701;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70710;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70715;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70715;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
-
-//! only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 32400;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
